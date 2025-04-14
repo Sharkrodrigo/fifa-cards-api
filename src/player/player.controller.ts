cbc9089b45dea6c09e5a2f1f07e3e56b42af0252
DaviRodrigo <davirodrigo2524@gmail.com>
2025-04-14 12:19:08 -0300

    feat: criação do service e dos controllers

--- src/player/player.controller.ts
+++ src/player/player.controller.ts
@@ -1,4 +1,54 @@
-import { Controller } from '@nestjs/common';
+// src/player/player.controller.ts
+import { Controller, Get, Post, Body, Param, Res, Put, Delete, NotFoundException } from '@nestjs/common';
+import { PlayerService } from './player.service';
+import { Response } from 'express';
 
-@Controller('player')
-export class PlayerController {}
+@Controller('players')
+export class PlayerController {
+  constructor(private readonly service: PlayerService) {}
+
+  @Get()
+  findAll() {
+    return this.service.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.service.findOne(+id);
+  }
+
+  @Post()
+  create(@Body() data) {
+    return this.service.create(data);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() data) {
+    return this.service.update(+id, data);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.service.delete(+id);
+  }
+
+  @Get(':id/card-html')
+async getCardHtml(@Param('id') id: string, @Res() res: Response) {
+  const player = await this.service.findOne(+id);
+  if (!player) throw new NotFoundException(`Player com ID ${id} não encontrado`);
+
+  const html = await this.service.renderHtml(player);
+  res.setHeader('Content-Type', 'text/html');
+  res.send(html);
+}
+
+@Get(':id/card')
+async getCardImage(@Param('id') id: string, @Res() res: Response) {
+  const player = await this.service.findOne(+id);
+  if (!player) throw new NotFoundException(`Player com ID ${id} não encontrado`);
+
+  const buffer = await this.service.gerarCardImagem(player);
+  res.setHeader('Content-Type', 'image/png');
+  res.send(buffer);
+}
+}
--- src/player/player.service.ts
+++ src/player/player.service.ts
@@ -1,4 +1,71 @@
+// src/player/player.service.ts
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { NotFoundException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as puppeteer from 'puppeteer';
+import { Player } from './player';
 
 @Injectable()
-export class PlayerService {}
+export class PlayerService {
+    constructor(
+        @InjectRepository(Player)
+        private repo: Repository<Player>,
+    ) { }
+
+    findAll() {
+        return this.repo.find();
+    }
+
+    findOne(id: number) {
+        return this.repo.findOneBy({ id });
+    }
+
+    create(data: Partial<Player>) {
+        const player = this.repo.create(data);
+        return this.repo.save(player);
+    }
+
+    update(id: number, data: Partial<Player>) {
+        return this.repo.update(id, data);
+    }
+
+    delete(id: number) {
+        return this.repo.delete(id);
+    }
+
+    async renderHtml(player: Player) {
+        const templatePath = path.join(__dirname, 'templates/card.html');
+        let html = fs.readFileSync(templatePath, 'utf8');
+
+        html = html
+            .replace(/{{nome}}/g, player.nome)
+            .replace(/{{fotourl}}/g, player.fotourl)
+            .replace(/{{posicao}}/g, player.posicao)
+            .replace(/{{overall}}/g, String(player.overall))
+            .replace(/{{stats\.pac}}/g, String(player.stats.pac))
+            .replace(/{{stats\.sho}}/g, String(player.stats.sho))
+            .replace(/{{stats\.pas}}/g, String(player.stats.pas))
+            .replace(/{{stats\.dri}}/g, String(player.stats.dri))
+            .replace(/{{stats\.def}}/g, String(player.stats.def))
+            .replace(/{{stats\.phy}}/g, String(player.stats.phy));
+
+        return html;
+    }
+
+    async gerarCardImagem(player: Player): Promise<Buffer> {
+        const html = await this.renderHtml(player);
+      
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.setContent(html);
+      
+        const buffer = await page.screenshot() as Buffer;
+        await browser.close();
+      
+        return buffer;
+      }
+      
+}
