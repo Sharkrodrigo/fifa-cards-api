ce81b9dbf3cd20ee49f71dee500231e69caee1ef
DaviRodrigo <davirodrigo2524@gmail.com>
2025-04-14 12:14:15 -0300

    feat: modelagem do player e template html do card

--- src/player/player.ts
+++ src/player/player.ts
@@ -1,6 +1,29 @@
-import { Entity } from "typeorm"
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
 @Entity()
 export class Player {
+  @PrimaryGeneratedColumn()
+  id: number;
 
+  @Column()
+  nome: string;
+
+  @Column()
+  fotourl: string;
+
+  @Column()
+  posicao: string;
+
+  @Column()
+  overall: number;
+
+  @Column('json')
+  stats: {
+    pac: number;
+    sho: number;
+    pas: number;
+    dri: number;
+    def: number;
+    phy: number;
+  };
 }
