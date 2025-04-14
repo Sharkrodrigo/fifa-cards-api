35c3a83da2fd70c0cbc5c364326399bab524a3df
DaviRodrigo <davirodrigo2524@gmail.com>
2025-04-14 13:33:31 -0300

    colocando url do banco diretamente

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,18 @@ import { PlayerController } from './player/player.controller';
 import { PlayerService } from './player/player.service';
 import { Player } from './player/player';
 
-@Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      url: process.env.DATABASE_URL,
-      autoLoadEntities: true,
-      entities: [Player],
-      synchronize: true,
-    }),
-    TypeOrmModule.forFeature([Player]),
-  ],
-  controllers: [PlayerController],
-  providers: [PlayerService],
-})
+  @Module({
+    imports: [
+      TypeOrmModule.forRoot({
+        type: 'postgres',
+        url: 'postgresql://postgres:bnOwwNmErqbeBDdQlVAdkJkVAtFvKfUL@ballast.proxy.rlwy.net:49322/railway',
+        autoLoadEntities: true,
+        entities: [Player],
+        synchronize: true,
+      }),
+      TypeOrmModule.forFeature([Player]),
+    ],
+    controllers: [PlayerController],
+    providers: [PlayerService],
+  })
 export class AppModule {}
