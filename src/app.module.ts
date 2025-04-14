e3aee08315a981ee7d3df2e6290729634df7bfbe
DaviRodrigo <davirodrigo2524@gmail.com>
2025-04-14 15:01:49 -0300

    ajuste no app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,19 +3,23 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { PlayerController } from './player/player.controller';
 import { PlayerService } from './player/player.service';
 import { Player } from './player/player';
+import { UserModule } from './user/user.module';
+import { User } from './user/user';
 
-  @Module({
-    imports: [
-      TypeOrmModule.forRoot({
-        type: 'postgres',
-        url: 'postgresql://postgres:bnOwwNmErqbeBDdQlVAdkJkVAtFvKfUL@ballast.proxy.rlwy.net:49322/railway',
-        autoLoadEntities: true,
-        entities: [Player],
-        synchronize: true,
-      }),
-      TypeOrmModule.forFeature([Player]),
-    ],
-    controllers: [PlayerController],
-    providers: [PlayerService],
-  })
-export class AppModule {}
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: 'postgresql://postgres:bnOwwNmErqbeBDdQlVAdkJkVAtFvKfUL@ballast.proxy.rlwy.net:49322/railway',
+      autoLoadEntities: true,
+
+      entities: [Player, User],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([Player, User]),
+    UserModule,
+  ],
+  controllers: [PlayerController],
+  providers: [PlayerService],
+})
+export class AppModule { }
