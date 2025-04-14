e754e569a3a9dd0c01d54ef221714c589d6a1677
DaviRodrigo <davirodrigo2524@gmail.com>
2025-04-14 14:59:53 -0300

    ajuste no ambiente

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,25 +3,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { PlayerController } from './player/player.controller';
 import { PlayerService } from './player/player.service';
 import { Player } from './player/player';
-import { UserModule } from './user/user.module';
-import { User } from './user/user';
 
-@Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '12345',
-      database: 'mercefut',
-      entities: [Player, User],
-      synchronize: true,
-    }),
-    TypeOrmModule.forFeature([Player, User]),
-    UserModule,
-  ],
-  controllers: [PlayerController],
-  providers: [PlayerService],
-})
+  @Module({
+    imports: [
+      TypeOrmModule.forRoot({
+        type: 'postgres',
+        url: 'postgresql://postgres:bnOwwNmErqbeBDdQlVAdkJkVAtFvKfUL@ballast.proxy.rlwy.net:49322/railway',
+        autoLoadEntities: true,
+        entities: [Player],
+        synchronize: true,
+      }),
+      TypeOrmModule.forFeature([Player]),
+    ],
+    controllers: [PlayerController],
+    providers: [PlayerService],
+  })
 export class AppModule {}
